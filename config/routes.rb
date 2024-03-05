17c2f820ec7246a08bc587c78d47448d7bf68cf6
moneshpx <monesh.s@pixoatic.com>
2024-03-05 08:21:25 +0530

    create the login and post crud

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  authenticated :user do
+    root to: 'posts#index', as: :authenticated_root
+  end
+
+  root "posts#index"
+
+  resources :posts
+end
